8efb531aabdc40222b85249e69fb6e70672af823
vapvin <tpehdj12@daum.net>
2019-11-16 20:45:25 +0900

    PHP Pparamiter

---
+++ paramiter.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+</head>
+
+<body>
+    안녕하세요. <?php echo $_GET['name']; ?>님
+</body>
+
+</html>
