9ae6cf5a706317952d38ab7a00009388cf2405be
vapvin <tpehdj12@daum.net>
2019-11-19 15:01:49 +0900

    Every Study To day is a PHP And Python

---
+++ boolean.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Boolean</title>
+</head>
+<body>
+    <h1>Comparsion Operators &amp; Boolean Data Type</h1>
+
+    <h2>1==1</h2>
+    <?php
+        var_dump(1==1);
+    ?>
+
+    <h2>1 > 2</h2>
+    <?php
+        var_dump(1 > 2)
+    ?>
+
+    <h2>1 >= 1</h2>
+    <?php
+        var_dump(1 >= 1)
+    ?>
+</body>
+</html>
---
+++ conditional.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Conditional</title>
+</head>
+<body>
+    <h1>Conditional</h1>
+    <h2>If</h2>
+    <?php
+        echo "1<br>";
+        if (true) {
+            echo "2-1<br>";
+        } else {
+            echo "2-2<br>";
+        }
+        echo "3<br>";
+    ?>
+</body>
+</html>
