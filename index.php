db3718716ad568162d9742f1068eb1c459b37ed7
vapvin <tpehdj12@daum.net>
2019-12-01 13:45:31 +0900

    Study Sunday

--- index.php
+++ index.php
@@ -1,3 +1,37 @@
+<?php
+function print_title()
+{
+    if (isset($_GET['id'])) {
+        echo $_GET['id'];
+    } else {
+        echo "Welcome";
+    }
+};
+
+function print_content()
+{
+    if (isset($_GET['id'])) {
+        echo file_get_contents("data/" . $_GET['id']);
+    } else {
+        echo "Hello World";
+    }
+};
+
+function print_list()
+{
+    $list = scandir('./data');
+    $i = 0;
+    while ($i < count($list)) {
+        if ($list[$i] != '.') {
+            if ($list[$i] != '..') {
+                echo "<li><a href=\"index.php?id=$list[$i]\">$list[$i]</a></li>\n";
+            }
+        }
+        $i = $i + 1;
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -11,27 +45,19 @@
 <body>
     <h1>Web</h1>
     <ol>
-        <li><a href="index.php?id=HTML">HTML</a></li>
-        <li><a href="index.php?id=CSS">CSS</a></li>
-        <li><a href="index.php?id=JavaScript">JavaScript</a></li>
+        <?php
+        print_list();
+        ?>
     </ol>
 
     <h2>
         <?php
-           if(isset($_GET['id'])) {
-               echo $_GET['id'];
-           } else {
-               echo "Welcome";
-           }
+        print_title();
         ?>
     </h2>
     <p>
         <?php
-            if(isset($_GET['id'])){
-                echo file_get_contents("data/".$_GET['id']);
-            } else {
-                echo "Hello World";
-            }
+        print_content();
         ?>
     </p>
 
