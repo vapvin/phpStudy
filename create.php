2a6fe03c5d670469fcbe021daa003341efc5a3e3
vapvin <tpehdj12@daum.net>
2019-12-01 13:49:50 +0900

    create process

--- create.php
+++ create.php
@@ -50,16 +50,19 @@ function print_list()
         ?>
     </ol>
     <a href="create.php">Create</a>
-    <h2>
-        <?php
-        print_title();
-        ?>
-    </h2>
-    <p>
-        <?php
-        print_content();
-        ?>
-    </p>
+    <form action="create_process.php" method="post">
+        <p>
+            <input type="text" name="title" placeholder="Title">
+        </p>
+
+        <p>
+            <textarea name="description" id="" cols="30" rows="10" placeholder="Description"></textarea>
+        </p>
+
+        <p>
+            <input type="submit">
+        </p>
+    </form>
 
 </body>
 
