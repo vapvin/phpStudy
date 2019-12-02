464a0a37b95073dbf0b4e76f4e925afee2f19c08
vapvin <tpehdj12@daum.net>
2019-12-02 23:55:02 +0900

    typedef enum

--- study.c
+++ study.c
@@ -3,3 +3,24 @@
 #include<conio.h>
 #include<time.h>
 #include<stdlib.h>
+
+typedef enum { NOCURSOR, SOLIDCURSOR, NORMALCURSOR } CURSOR_TYPE; 
+     CONSOLE_CURSOR_INFO CurInfo;
+ 
+     switch (c) {
+     case NOCURSOR:
+          CurInfo.dwSize=1;
+          CurInfo.bVisible=FALSE;
+          break;
+     case SOLIDCURSOR:
+          CurInfo.dwSize=100;
+          CurInfo.bVisible=TRUE;
+          break;
+     case NORMALCURSOR:
+          CurInfo.dwSize=20;
+          CurInfo.bVisible=TRUE;
+          break;
+     }
+     SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE),&CurInfo);
+}
+ 
