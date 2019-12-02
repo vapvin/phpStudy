c4b9676bbf1a14870b7748e187781bdda13d260a
vapvin <tpehdj12@daum.net>
2019-12-02 23:55:57 +0900

    Fuck the hard game

--- study.c
+++ study.c
@@ -24,3 +24,31 @@ typedef enum { NOCURSOR, SOLIDCURSOR, NORMALCURSOR } CURSOR_TYPE;
      SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE),&CurInfo);
 }
  
+
+ int main(){
+    int i;
+    
+    srand((unsigned)time(NULL)); 
+    setcursortype(NOCURSOR); 
+    title();
+    reset(); 
+    
+    while(1){
+        for(i=0;i<5;i++){  
+            check_key();
+            draw_main(); 
+            Sleep(speed); 
+            if(crush_on&&check_crush(bx,by+1, b_rotation)==false) Sleep(100);
+                                               
+            if(space_key_on==1) { 
+                space_key_on=0;
+                break;
+            }
+        }
+        drop_block(); 
+        check_level_up();
+        check_game_over();
+        if(new_block_on==1) new_block(); 
+    }
+}
+ 
